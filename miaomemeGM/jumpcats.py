8d933715b365d2807cb82ce45e73ae7166530716
焦糖酒 <148602073+drunksweet@users.noreply.github.com>
2024-05-26 07:12:36 +0800

    version3

---
+++ miaomemeGM/jumpcats.py
@@ -0,0 +1,161 @@
+
+import pygame
+import random
+
+
+
+
+class Player_short(object):
+    """
+    player对象
+    """
+
+    def __init__(self,screen):
+        self.image = pygame.image.load('src/drawable/test2.jpg').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (37.5,112.5)
+        self.vel_y = 0
+        self.jumped = False
+        self.screen = screen
+
+    def move(self,init_x):
+        self.rect.x = init_x
+        self.screen.blit(self.image, self.rect)
+        self.vel_y = -10
+        self.jumped = True
+
+    def randommove(self):
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s]:
+            list1 = [20,  30, -10,-25]
+            self.move(random.choice(list1))
+            self.vel_y = -10
+            self.jumped = True
+
+
+
+    def jump(self):
+        y_move = 0
+
+        # 添加角色重力（跳跃之后自然下落）
+        self.vel_y += 1.2
+
+        if self.vel_y > 10:
+            self.vel_y = 10
+
+        y_move += self.vel_y
+
+        self.rect.y += y_move
+
+        # 控制人物的最低位置
+        if self.rect.bottom > 150:
+            self.rect.bottom = 150
+
+        # 绘制人物
+        self.screen.blit(self.image, self.rect)
+
+class Player_double(object):
+    """
+    player对象
+    """
+
+    def __init__(self,screen):
+        self.image = pygame.image.load('src/drawable/test3.jpg').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (112.5,112.5)
+        self.vel_y = 0
+        self.jumped = False
+        self.screen = screen
+
+
+    def move(self,init_x):
+        self.rect.x = init_x
+        self.screen.blit(self.image, self.rect)
+        self.vel_y = -14
+        self.jumped = True
+
+    def randommove(self):
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            list1 = [-20,-30,  30, 20]
+            self.move(random.choice(list1))
+            self.vel_y = -14
+            self.jumped = True
+
+
+
+    def jump(self):
+        y_move = 0
+
+        # 添加角色重力（跳跃之后自然下落）
+        self.vel_y += 1.2
+
+        if self.vel_y > 10:
+            self.vel_y = 10
+
+        y_move += self.vel_y
+
+        self.rect.y += y_move
+
+        # 控制人物的最低位置
+        if self.rect.bottom > 150:
+            self.rect.bottom = 150
+
+        # 绘制人物
+        self.screen.blit(self.image, self.rect)
+
+
+class Player_long(object):
+    """
+    player对象
+    """
+
+    def __init__(self,screen):
+        self.image = pygame.image.load('src/drawable/test3.jpg').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (187.5,112.5)
+        self.vel_y = 0
+        self.jumped = False
+        self.screen = screen
+
+    def move(self,init_x):
+        self.rect.x = init_x
+        self.screen.blit(self.image, self.rect)
+        self.vel_y = -16
+        self.jumped = True
+
+    def randommove(self):
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            list1 = [-20,-30,  30, 20]
+            self.move(random.choice(list1))
+            self.vel_y = -16
+            self.jumped = True
+
+
+
+    def jump(self):
+        y_move = 0
+
+        # 添加角色重力（跳跃之后自然下落）
+        self.vel_y += 1.2
+
+        if self.vel_y > 10:
+            self.vel_y = 10
+
+        y_move += self.vel_y
+
+        self.rect.y += y_move
+
+        # 控制人物的最低位置
+        if self.rect.bottom > 150:
+            self.rect.bottom = 150
+
+        # 绘制人物
+        self.screen.blit(self.image, self.rect)
+
+
+#######jumpcats
--- miaomemeGM/tangdemo4.py
+++ miaomemeGM/tangdemo4.py
@@ -3,7 +3,7 @@ import pygame
 import sys
 import random
 from pygame.sprite import Sprite
-
+from  jumpcats import *
 
 class COLOR:
     BLACK = (0, 0, 0)
@@ -12,7 +12,8 @@ class COLOR:
     GREEN = (0, 255, 0)
     GREY = (190, 190, 190)
     YELLOW = (255, 255, 0)
-    NAVAJOWHITE = (255,222,173)
+    NAVAJOWHITE = (255, 222, 173)
+    NAVAJOWHITE4 = (139, 121, 94)
 
 
 class WINDOW:
@@ -36,6 +37,185 @@ class BLOCK:
             return random.randint(1, 3)
 
 
+
+class Block(Sprite):
+    def __init__(self, color, x, y, is_black):
+        super().__init__()
+        self.image = pygame.Surface((BLOCK.WIDTH, BLOCK.HEIGHT))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x, y)
+        self.is_black = is_black
+        self.has_clicked = (color == COLOR.WHITE)
+        self.form = BLOCK.FORM.NORM
+        self.has_changed = False
+        self.last_clicked_time = -1
+        self.last_y = 0
+        self.key = pygame.K_0
+        self.music = None
+
+    def toLong(self, h, color):
+        x, y = self.rect.bottomleft
+        self.image = pygame.Surface((BLOCK.WIDTH, h * BLOCK.HEIGHT))
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.bottomleft = (x, y)
+
+    def move(self, vel):
+        self.rect.y += vel
+
+    def clicked(self, mouse_x=0,color=COLOR.GREY):
+        if self.has_clicked or not self.is_black:
+            return False
+        elif self.form == BLOCK.FORM.NORM:
+            self.image.fill(color)
+            self.has_clicked = True
+            self.music = chooseShortSound(judge_column(mouse_x))
+        elif self.form == BLOCK.FORM.LONG:
+            self.has_clicked = True
+            self.music = chooseLongSound(judge_column(mouse_x))
+        elif self.form == BLOCK.FORM.SOLID:
+            if self.last_clicked_time == -1:
+                self.has_clicked = False
+                self.last_clicked_time = pygame.time.get_ticks()
+            else:
+                if pygame.time.get_ticks() - self.last_clicked_time < 300:
+                    self.has_clicked = True
+                    self.image.fill(color)
+                    self.music = chooseShortSound(judge_column(mouse_x))
+                self.last_clicked_time = -1
+            self.last_clicked_time = pygame.time.get_ticks()
+        return True
+
+    def is_changed(self,mouse_y):
+        if self.form == BLOCK.FORM.LONG:
+            if not self.has_changed:
+                # mouse_x, mouse_y = pygame.mouse.get_pos()
+                distance_from_bottom = self.rect.bottom - mouse_y
+                # 创建一个新的Surface对象，将点击位置以上的区域填充为透明
+                transparent_surface = pygame.Surface((self.rect.width, distance_from_bottom))
+                transparent_surface.set_colorkey(COLOR.GREEN)  # 设置透明颜色
+                transparent_surface.set_alpha(70)  # 设置透明度
+                transparent_surface.fill(COLOR.GREY)
+                # 计算透明 Surface 的位置，使其位于黑块底部
+                transparent_y = (mouse_y - self.rect.top)
+                # 将透明区域绘制到黑块Surface上
+                self.image.blit(transparent_surface, (0, transparent_y))
+                self.last_y = mouse_y
+            if not pygame.key.get_pressed()[self.key] or mouse_y <= self.rect.top:
+                self.has_changed = True
+        elif self.form == BLOCK.FORM.NORM:
+            self.last_y = self.rect.top
+            self.has_changed = True
+        elif self.form == BLOCK.FORM.SOLID:
+            if self.has_clicked:
+                self.last_y = self.rect.bottom + BLOCK.HEIGHT
+                self.has_changed = True
+
+        return self.has_changed
+
+
+def get_black_block(pos_y=[0], form=0):
+    if form == 0:
+        form = BLOCK.FORM.random()
+    x_pos = random.randint(0, WINDOW.X_LEN-1)
+    block = Block(COLOR.BLACK, x_pos * BLOCK.WIDTH, pos_y[0], True)
+    pos_y[0] -= BLOCK.HEIGHT;
+    y_len = 0
+    if form == BLOCK.FORM.LONG:
+        y_len = random.randint(2, 5)
+        pos_y[0] -= (y_len - 1) * BLOCK.HEIGHT
+        block.toLong(y_len, COLOR.GREEN)
+    elif form == BLOCK.FORM.SOLID:
+        block.image.fill(COLOR.YELLOW)
+
+    block.form = form
+    print(pos_y, y_len, block.rect)
+    return block
+
+
+# 黑块末尾
+end_pos_y = [BLOCK.HEIGHT*3]
+# 被选中的黑块
+selected_block = None
+
+
+def judge_column(pos_x):
+    column_positions = {
+        WINDOW.WIDTH * 0.125 / 2: 1,
+        WINDOW.WIDTH * (0.125 + 0.25) / 2: 2,
+        WINDOW.WIDTH * (0.25 + 0.375) / 2: 3,
+        WINDOW.WIDTH * (0.375 + 0.5) / 2: 4,
+        WINDOW.WIDTH * (0.5 + 0.625) / 2: 5,
+        WINDOW.WIDTH * (0.625 + 0.75) / 2: 6,
+        WINDOW.WIDTH * (0.75 + 0.875) / 2: 7,
+        WINDOW.WIDTH * (0.875 + 1) / 2: 8
+    }
+
+    return column_positions.get(pos_x, None)
+
+def chooseShortSound(col):
+    if col==1:
+        return short01
+    elif col==2:
+        return short02
+    elif col==3:
+        return short03
+    elif col==4:
+        return short04
+    elif col==5:
+        return short05
+    elif col==6:
+        return short06
+    elif col==7:
+        return  short07
+    elif col==8:
+        return short08
+
+def chooseLongSound(col):
+    if col == 1:
+        return long01
+    elif col == 2:
+        return long02
+    elif col == 3:
+        return long03
+    elif col == 4:
+        return long04
+    elif col == 5:
+        return long05
+    elif col == 6:
+        return long06
+    elif col == 7:
+        return long07
+    elif col == 8:
+        return long08
+
+def judge(block):
+    form = block.form
+    if form==BLOCK.FORM.NORM:
+        # player_short.jump()
+        player_short.move(block.rect.x)
+    elif form==BLOCK.FORM.LONG:
+        player_long.move(block.rect.x)
+    elif form==BLOCK.FORM.SOLID:
+        player_double.move(block.rect.x)
+def init():
+    global running, blocks_group, score, start, game_over, level, v_n, v_list, game_over_has_played
+    # pygame.mixer.music.play(-1)
+    running = True
+    blocks_group = pygame.sprite.Group()
+    score = 0
+    start = False
+    game_over = False
+    game_over_has_played = False
+    level = score // 4
+    v_n = 0
+    v_list = ([int(1.5 + level * 0.125)] * int((int(2.5 + level * 0.125) - (1.5 + level * 0.125)) // 0.125) +
+              [int(2.5 + level * 0.125)] * int(((1.5 + level * 0.125) - int(1.5 + level * 0.125)) // 0.125))
+
+    # for _ in range(WINDOW.Y_LEN + 1):
+    #     # 添加新块
+    #     blocks_group.add(get_black_block(pos_y=end_pos_y))
 # 初始化 Pygame
 pygame.init()
 
@@ -72,184 +252,25 @@ font = pygame.font.SysFont('arial', 20)
 clock = pygame.time.Clock()
 
 
-class Block(Sprite):
-    def __init__(self, color, x, y, is_black):
-        super().__init__()
-        self.image = pygame.Surface((BLOCK.WIDTH, BLOCK.HEIGHT))
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.rect.bottomleft = (x, y)
-        self.is_black = is_black
-        self.has_clicked = (color == COLOR.WHITE)
-        self.form = BLOCK.FORM.NORM
-        self.has_changed = False
-        self.last_clicked_time = -1
-        self.last_y = 0
-        self.key = pygame.K_0
-
-    def toLong(self, h, color):
-        x, y = self.rect.bottomleft
-        self.image = pygame.Surface((BLOCK.WIDTH, h * BLOCK.HEIGHT))
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
-        self.rect.bottomleft = (x, y)
-
-    def move(self, vel):
-        self.rect.y += vel
-
-    def clicked(self, mouse_x=0,color=COLOR.GREY):
-        if self.has_clicked or not self.is_black:
-            return False
-        elif self.form == BLOCK.FORM.NORM:
-            self.image.fill(color)
-            self.has_clicked = True
-            chooseShortSound(judge_column(mouse_x))
-        elif self.form == BLOCK.FORM.LONG:
-            self.has_clicked = True
-            chooseLongSound(judge_column(mouse_x))
-        elif self.form == BLOCK.FORM.SOLID:
-            if self.last_clicked_time == -1:
-                self.has_clicked = False
-                self.last_clicked_time = pygame.time.get_ticks()
-            else:
-                if pygame.time.get_ticks() - self.last_clicked_time < 300:
-                    self.has_clicked = True
-                    self.image.fill(color)
-                    chooseShortSound(judge_column(mouse_x))
-                self.last_clicked_time = -1
-            self.last_clicked_time = pygame.time.get_ticks()
-        return True
-
-    def is_changed(self,mouse_y):
-        if self.form == BLOCK.FORM.LONG:
-            if not self.has_changed:
-                # mouse_x, mouse_y = pygame.mouse.get_pos()
-                distance_from_bottom = self.rect.bottom - mouse_y
-                # 创建一个新的Surface对象，将点击位置以上的区域填充为透明
-                transparent_surface = pygame.Surface((self.rect.width, distance_from_bottom))
-                transparent_surface.set_colorkey(COLOR.GREEN)  # 设置透明颜色
-                transparent_surface.set_alpha(70)  # 设置透明度
-                transparent_surface.fill(COLOR.GREY)
-                # 计算透明 Surface 的位置，使其位于黑块底部
-                transparent_y = (mouse_y - self.rect.top)
-                # 将透明区域绘制到黑块Surface上
-                self.image.blit(transparent_surface, (0, transparent_y))
-                self.last_y = mouse_y
-            if not pygame.key.get_pressed()[self.key]:
-                self.has_changed = True
-        elif self.form == BLOCK.FORM.NORM:
-            self.last_y = self.rect.top
-            self.has_changed = True
-        elif self.form == BLOCK.FORM.SOLID:
-            if self.has_clicked:
-                self.last_y = self.rect.bottom + BLOCK.HEIGHT
-                self.has_changed = True
-
-        return self.has_changed
-
-
-def get_black_block(pos_y=[0], form=0):
-    if form == 0:
-        form = BLOCK.FORM.random()
-    x_pos = random.randint(0, WINDOW.X_LEN-1)
-    block = Block(COLOR.BLACK, x_pos * BLOCK.WIDTH, pos_y[0], True)
-    pos_y[0] -= BLOCK.HEIGHT;
-    y_len = 0
-    if form == BLOCK.FORM.LONG:
-        y_len = random.randint(2, 5)
-        pos_y[0] -= (y_len - 1) * BLOCK.HEIGHT
-        block.toLong(y_len, COLOR.GREEN)
-    elif form == BLOCK.FORM.SOLID:
-        block.image.fill(COLOR.YELLOW)
-
-    block.form = form
-    print(pos_y, y_len, block.rect)
-    return block
-
-
-# 黑块末尾
-end_pos_y = [BLOCK.HEIGHT*2]
-# 被选中的黑块
-selected_block = None
-
-
-def judge_column(pos_x):
-    column_positions = {
-        WINDOW.WIDTH * 0.125 / 2: 1,
-        WINDOW.WIDTH * (0.125 + 0.25) / 2: 2,
-        WINDOW.WIDTH * (0.25 + 0.375) / 2: 3,
-        WINDOW.WIDTH * (0.375 + 0.5) / 2: 4,
-        WINDOW.WIDTH * (0.5 + 0.625) / 2: 5,
-        WINDOW.WIDTH * (0.625 + 0.75) / 2: 6,
-        WINDOW.WIDTH * (0.75 + 0.875) / 2: 7,
-        WINDOW.WIDTH * (0.875 + 1) / 2: 8
-    }
-
-    return column_positions.get(pos_x, None)
-
-def chooseShortSound(col):
-    if col==1:
-        return short01.play()
-    elif col==2:
-        return short02.play()
-    elif col==3:
-        return short03.play()
-    elif col==4:
-        return short04.play()
-    elif col==5:
-        return short05.play()
-    elif col==6:
-        return short06.play()
-    elif col==7:
-        return  short07.play()
-    elif col==8:
-        return short08.play()
-
-def chooseLongSound(col):
-    if col == 1:
-        return long01.play()
-    elif col == 2:
-        return long02.play()
-    elif col == 3:
-        return long03.play()
-    elif col == 4:
-        return long04.play()
-    elif col == 5:
-        return long05.play()
-    elif col == 6:
-        return long06.play()
-    elif col == 7:
-        return long07.play()
-    elif col == 8:
-        return long08.play()
-
-
-def init():
-    global running, blocks_group, score, start, game_over, level, v_n, v_list, game_over_has_played
-    # pygame.mixer.music.play(-1)
-    running = True
-    blocks_group = pygame.sprite.Group()
-    score = 0
-    start = False
-    game_over = False
-    game_over_has_played = False
-    level = score // 4
-    v_n = 0
-    v_list = ([int(1.5 + level * 0.125)] * int((int(2.5 + level * 0.125) - (1.5 + level * 0.125)) // 0.125) +
-              [int(2.5 + level * 0.125)] * int(((1.5 + level * 0.125) - int(1.5 + level * 0.125)) // 0.125))
-
-    # for _ in range(WINDOW.Y_LEN + 1):
-    #     # 添加新块
-    #     blocks_group.add(get_black_block(pos_y=end_pos_y))
-
 
 init()
 
+######### jumpcats
+bg = pygame.image.load("src/drawable/test1.jpg").convert()
+player_short = Player_short(screen)
+player_double = Player_double(screen)
+player_long = Player_long(screen)
+
+
+########
 mouse_y = WINDOW.HEIGHT*0.95
 
 while running:
+
     clock.tick(60)
     screen.fill(COLOR.WHITE)
+
+
     # 设置块的速度
 
     if score // 4 > level:
@@ -258,7 +279,8 @@ while running:
                   [int(2.5 + level * 0.125)] * int(((1.5 + level * 0.125) - int(1.5 + level * 0.125)) // 0.125))
 
     if start:
-        v = v_list[v_n]
+        v = 1
+        # v_list[v_n]
         v_n += 1
         if v_n == 8:
             v_n = 0
@@ -267,14 +289,16 @@ while running:
             block.move(v)
         # 更新末尾位置
         end_pos_y[0] += v
-        if end_pos_y[0] > BLOCK.HEIGHT*2:
-            end_pos_y[0] = BLOCK.HEIGHT*2
+        if end_pos_y[0] > BLOCK.HEIGHT*3:
+            end_pos_y[0] = BLOCK.HEIGHT*3
     else:
         v = 0
 
     # 末尾为0，则添加新的方块到最后
-    if end_pos_y[0] == BLOCK.HEIGHT*2:
-        blocks_group.add(get_black_block(pos_y=end_pos_y))
+    if end_pos_y[0] == BLOCK.HEIGHT*3:
+        top_block = get_black_block(pos_y=end_pos_y)
+        blocks_group.add(top_block)
+        judge(top_block)
 
 
     # 检查最底下的方块是否超出边界
@@ -326,12 +350,17 @@ while running:
                 mouse_x = 0
                 
             for block in blocks_group.sprites():
-                if block.rect.collidepoint(mouse_x,mouse_y):
+                # if block.rect.collidepoint(mouse_x,mouse_y):
+                if block.rect.x <= mouse_x and mouse_x <= block.rect.x + BLOCK.WIDTH and abs(block.rect.bottom-mouse_y) <= BLOCK.HEIGHT/2:
                     if selected_block == block:
                         block.clicked(mouse_x)
+                        if block.has_clicked:
+                            block.music.play()
                     elif selected_block == None and not block.has_clicked:
                         selected_block = block
                         block.clicked(mouse_x)
+                        if block.has_clicked:
+                            block.music.play(-1 if block.form==BLOCK.FORM.LONG else 0)
                         block.key = event.key
                     # thread = threading.Thread(target=transparent_block, args=(block,))
                     # thread.start()
@@ -346,17 +375,33 @@ while running:
     if selected_block != None:
         if selected_block.is_changed(mouse_y):
             score += (selected_block.rect.bottom - selected_block.last_y) // BLOCK.HEIGHT
+            if selected_block.form == BLOCK.FORM.LONG:
+                selected_block.music.stop()
             selected_block = None
+
         # score_sound.play(0)
 
     # 绘制屏幕
-    screen.fill(COLOR.NAVAJOWHITE)
+    # screen.fill(COLOR.NAVAJOWHITE)
     blocks_group.draw(screen)
+    #########jumpcats
+    # screen.blit(player_short.image, player_short.rect)
+    player_short.jump()
+    # player_short.jump()
+    # player_short.randommove()
+    #
+    player_double.jump()
+    # player_double.randommove()
+    #
+    player_long.jump()
+    # player_long.randommove()
+    #########
+
     pygame.draw.line(screen, [255, 20, 147], [0, mouse_y],[WINDOW.WIDTH, mouse_y], 5)
     pygame.draw.line(screen, [255, 20, 147], [WINDOW.WIDTH/2, 0], [WINDOW.WIDTH/2, WINDOW.HEIGHT], 5)
     pygame.draw.line(screen, [255, 20, 147], [0, BLOCK.HEIGHT*3], [WINDOW.WIDTH, BLOCK.HEIGHT*3], 5)
     pygame.draw.line(screen, [255, 20, 147], [0, BLOCK.HEIGHT*2], [WINDOW.WIDTH, BLOCK.HEIGHT*2], 5)
-
+    # pygame.draw.rect(screen, COLOR.NAVAJOWHITE4, (0,BLOCK.HEIGHT*2,WINDOW.WIDTH,BLOCK.HEIGHT))
     # if game_over:
     #     pygame.mixer.music.stop()
     #     if not game_over_has_played:
